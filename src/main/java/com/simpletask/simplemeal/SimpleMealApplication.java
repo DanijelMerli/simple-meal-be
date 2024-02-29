c2f5ff0967379ad1945197e479eefefa8dc68048
danijel-merli <danijel@presles.co.za>
2024-02-29 10:16:15 +0100

    Initial commit

---
+++ src/main/java/com/simpletask/simplemeal/SimpleMealApplication.java
@@ -0,0 +1,13 @@
+package com.simpletask.simplemeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpletask/simplemeal/SimpleMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpletask.simplemeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
