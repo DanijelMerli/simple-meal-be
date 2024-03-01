6d39011e38db6b105daf3e3d977be6f5b161ed59
Olivera Kljajić <kljajic435@gmail.com>
2024-03-01 14:21:16 +0100

    Added User modela and repo

---
+++ src/test/java/com/simpletask/simplemeal/model/User.java
@@ -0,0 +1,71 @@
+package com.simpletask.simplemeal.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User implements Serializable{
+	
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idUser;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String password;
+
+	public User() {
+	}
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
