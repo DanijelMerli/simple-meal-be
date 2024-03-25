17fa4b1c7776bf6c51b2bf2b8fbeb731a3b8fa90
BojanaZekanovic9 <bojana.zekanovic9@gmail.com>
2024-03-25 14:22:32 +0100

    Implemented functionality for the chosen one and sent email

---
+++ src/main/java/com/simpletask/simplemeal/service/IRoleService.java
@@ -0,0 +1,8 @@
+package com.simpletask.simplemeal.service;
+
+import com.simpletask.simplemeal.model.User;
+
+public interface IRoleService {
+    public void assignChosenOneRole(User user);
+    public void revertToUserRole(User user);
+}
---
+++ src/main/java/com/simpletask/simplemeal/service/RoleService.java
@@ -0,0 +1,24 @@
+package com.simpletask.simplemeal.service;
+
+import com.simpletask.simplemeal.model.Role;
+import com.simpletask.simplemeal.model.User;
+import com.simpletask.simplemeal.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService implements IRoleService{
+    @Autowired
+    private RoleRepository roleRepository;
+    @Override
+    public void assignChosenOneRole(User user) {
+        Role chosenOneRole = roleRepository.findByName("THE_CHOSEN_ONE");
+        user.setRole(chosenOneRole);
+    }
+
+    @Override
+    public void revertToUserRole(User user) {
+        Role userRole = roleRepository.findByName("ROLE_USER");
+        user.setRole(userRole);
+    }
+}
